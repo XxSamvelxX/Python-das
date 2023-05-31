b8963ef4a854913fe95be397248e553fb868b020
Samvel <samvel.m.y@tumo.org>
2023-05-31 20:44:48 +0400

    Das_27

---
+++ Das 27/Das 27.py
@@ -0,0 +1,22 @@
+from functools import reduce
+
+# # Ex 1
+# Given an array of integers, find the pair of
+# adjacent elements that has the largest product
+# and return that product.
+# Example:
+# input -> list_1 = [3, 6, -2, -5, 7, 3]
+# output -> 21
+
+# l_1 = [3, 6, -2, -5, 7, 3]
+# a = []
+# for i in range(0, len(l_1) - 1):
+#     if i < len(l_1):
+#         a.append(l_1[i] * l_1[i + 1])
+# print(a, "Maximum is ", max(a))
+
+
+# Ex 2
+# Write a program to generate this image and
+# save this image in computer (use ImageGrab)
+
